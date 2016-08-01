08de16b7a5136b8c7090a99f6bca89b8c27be1f9
Sergeev1745 <sergeev.be1@gmail.com>
2016-08-01 15:15:13 +0300

    added files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var firstNum = prompt('Введите число');
+console.log('число= ', firstNum);
+var secondNum = prompt('Введите степень');
+console.log('степень= ', secondNum);
+var result = Math.pow(firstNum, secondNum);
+console.log('результат= ', result);
